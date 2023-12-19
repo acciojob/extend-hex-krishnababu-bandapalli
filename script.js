3e34d347661b4e3eea6b8a3b961a4bb94ddb28ee
Krishnababu Banadapalli <117643540+krishnababu-bandapalli@users.noreply.github.com>
2023-12-19 15:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const extendHex = (shortHex) => {
-  // write your code here
+	let i = shortHex.length-3, isCapitalized = false;
+	let output = "#";
+	
+	while (i < shortHex.length) {
+		let asciiCode = shortHex[i].charCodeAt(0);
+		if (asciiCode >= 65 && asciiCode <= 90) {
+			isCapitalized = true;
+		}
+		output += shortHex[i] + shortHex[i];
+		i++
+	}
+
+	return isCapitalized ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
